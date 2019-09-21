4ecc351964b712032d155534d6c4bd57ef7922f3
Glitch (db-tr) <none>
2019-09-21 06:33:48 +0000

    ☄️🍢 Checkpoint ./html/sertifika.ejs:6732780/1022 ./html/ek/header.ejs:6732780/144 ./komutlar/sertifika-iste.js:6732780/1418

---
+++ komutlar/sertifika-iste.js
@@ -0,0 +1,37 @@
+const Discord = require('discord.js');
+
+
+exports.run = function(client, message, args) {
+    let type = args.slice(0).join(' ');
+    if (type.length < 1) return message.channel.send(
+new Discord.RichEmbed()
+.setColor('BLUE')
+.setDescription('YANLIŞ ŞEKİLDE GİRDİNİZ BOTUNUZUN İSMİNİ BELİRTİNİZ')).then(msg => msg.delete(5000));
+
+const embed = new Discord.RichEmbed()
+.setColor('BLUE')
+.setDescription(`SERTİFİKA BAŞVURUNUZU ALDIK`)
+message.channel.send(embed)
+const embed2 = new Discord.RichEmbed()
+.setColor("BLUE")
+.setDescription(`SERTİFİKA İSTEK`)
+.addField(`\nKULLANICI BİLGİLERİ`,` \nKULLANICI ID: ${message.author.id} \nKULLANICI ADI: ${message.author.username} \nKULLANICI TAGI: #${message.author.discriminator}`)
+.addField(`\nSUNUCU BİLGİLERİ`,` \nSUNUCU İSMİ: ${message.guild.name} \nSUNUCU ID: ${message.guild.id}`)
+.addField("BOT ADI", type)
+.setThumbnail(message.author.avatarURL)
+client.channels.get('624643138178383903').send(embed2); // Kanal ID 
+
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false, 
+  aliases: [],
+  permLevel: 0 
+};
+
+exports.help = {
+  name: 'sertifika-iste',
+  description: '',
+  usage: ''
+};
