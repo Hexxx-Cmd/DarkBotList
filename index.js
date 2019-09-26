1c86682070953bc5d6faff7f364bf053f15a12b3
Glitch (db-tr) <none>
2019-09-26 18:07:00 +0000

    👻🛥 Checkpoint ./index.js:6732780/311

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ client.ayarlar = {
   "renk": "#7289DA" //embedların rengini burdan alıo can sıkıntısdna yapılmış bişe falan fln
 };
 
-client.yetkililer = ["621749124890361866","404206792005124096","378218497571815435"] //tüm yetkililerin ıdleri gelcek array
-client.webyetkililer = ["621749124890361866","404206792005124096","378218497571815435"] //web yetkililerin ıdleri gelcek array
-client.sunucuyetkililer = ["621749124890361866","404206792005124096","378218497571815435"] //sunucu yetkililerin ıdleri gelcek array
+client.yetkililer = ["621749124890361866","404206792005124096","378218497571815435","615646611090964511"] //tüm yetkililerin ıdleri gelcek array
+client.webyetkililer = ["621749124890361866","404206792005124096","378218497571815435","615646611090964511"] //web yetkililerin ıdleri gelcek array
+client.sunucuyetkililer = ["621749124890361866","404206792005124096","378218497571815435","615646611090964511"] //sunucu yetkililerin ıdleri gelcek array
 
 //
 
