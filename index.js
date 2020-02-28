bbbdef3ff736d38525f8e1dce30c5e693c992988
Glitch (db-tr) <none>
2020-02-28 23:55:27 +0000

    🐓🎡 Checkpoint ./index.js:9017739/5344 ./warning.json:9017739/4

--- index.js
+++ index.js
@@ -132,6 +132,98 @@ fs.readdir(`./komutlar/`, (err, files) => {
 	}
 });
 
+const moment = require("moment"); // npm i moment
+let warning = JSON.parse(fs.readFileSync('./warning.json' , 'utf8'));
+
+client.on('message',message=>{
+if(message.author.bot || message.channel.type == "dm"||!message.channel.guild) return;
+let prefix = '!';
+if (!message.content.startsWith(prefix)) return;
+let command = message.content.split(" ")[0];
+command = command.slice(prefix.length);
+if(command == 'warn'){if(!message.member.hasPermission('MANAGE_GUILD')) return;
+if(!warning[message.guild.id]) warning[message.guild.id] = {warns:[]}
+let T = warning[message.guild.id].warns;
+let user = message.mentions.users.first();if(!user)return message.channel.send(`**:rolling_eyes: I can't find this member**`)
+let reason = message.content.split(" ").slice(2).join(" ");if(!reason) return message.channel.send(`**:rolling_eyes: Please specify a reason.**`)
+let W = warning[message.guild.id].warns;
+let ID = 0;let leng = 0;
+W.forEach(w =>{ID++;
+if(w.id !== undefined) leng++;
+})
+if(leng === 90) return message.channel.send(`** You Can't Give More than \`90\` Warns**, please reset the warn list.`)
+T.push({user:user.id,by:message.author.id,reason:reason,time:moment(Date.now()).format('llll'),id:ID+1})
+message.channel.send(`**✅ @${user.username} warned!**`);
+fs.writeFile("./warning.json", JSON.stringify(warning),(err)=>{if(err)console.error(err)});
+fs.writeFile("./warning.json", JSON.stringify(warning),(err)=>{if(err)console.error(err)});
+user.send(new Discord.RichEmbed().addField('**:warning: You were warned!**',reason)
+.setFooter(message.guild.name,message.guild.iconURL).setTimestamp().setColor('#fffe62'));return;}
+if(command == 'warnings') {
+if(!message.member.hasPermission('MANAGE_GUILD')) return;
+if(!warning[message.guild.id]) warning[message.guild.id] = {warns:[]}
+let count = 0;let page = message.content.split(" ")[1];if(!page || isNaN(page)) page = 1;if(page > 4) return message.channel.send('**Warnings are only recorded on 4 pages!**')
+let embed = new Discord.RichEmbed().setFooter(message.author.username,message.author.avatarURL)
+let W = warning[message.guild.id].warns;
+W.forEach(w =>{if(!w.id) return;count++;
+if(page == 1) {if(count > 24) return null
+let reason = w.reason;let user = w.user;let ID = w.id;let By = w.by;let time = w.time;
+embed.addField(`⏱ ${time}`,`Warn ID (**${ID}**) - By <@${By}>
+User: <@${user}>\n\`\`\`${reason}\`\`\``);
+if(count == 24) embed.addField('**:sparkles: More ?**',`${message.content.split(" ")[0]} 2`);
+}if(page == 2) {if(count <= 24) return null;if(count > 45) return null
+let reason = w.reason;let user = w.user;let ID = w.id;let By = w.by;let time = w.time;
+embed.addField(`⏱ ${time}`,`Warn ID (**${ID}**) - By <@${By}>
+User: <@${user}>\n\`\`\`${reason}\`\`\``);
+if(count == 45) embed.addField('**:sparkles: More ?**',`${message.content.split(" ")[0]} 3`);
+}if(page == 3) {if(count <= 45) return null;if(count > 69) return null
+let reason = w.reason;let user = w.user;let ID = w.id;let By = w.by;let time = w.time;
+embed.addField(`⏱ ${time}`,`Warn ID (**${ID}**) - By <@${By}>
+User: <@${user}>\n\`\`\`${reason}\`\`\``);
+if(count == 69) embed.addField('**:sparkles: More ?**',`${message.content.split(" ")[0]} 4`);
+}if(page == 4) {if(count <= 69) return null;if(count > 92) return null
+let reason = w.reason;let user = w.user;let ID = w.id;let By = w.by;let time = w.time;
+embed.addField(`⏱ ${time}`,`Warn ID (**${ID}**) - By <@${By}>
+User: <@${user}>\n\`\`\`${reason}\`\`\``);
+if(count == 64) embed.addField('**FULL**',`** **`);}});
+embed.setTitle(`**${count} Warnings** [ ${page}/4 ]`)
+message.channel.send(embed)};
+if(command == 'removewarn' || command == 'rm') {
+if(!message.member.hasPermission('MANAGE_GUILD')) return;
+if(!warning[message.guild.id]) warning[message.guild.id] = {warns:[]};
+let args = message.content.split(" ")[1];if(!args) return message.channel.send(
+`**:rolling_eyes: Please specify warning number or user mention or (all) to delete all warnings.**`);
+let user = message.mentions.members.first();
+if(user) {
+let C = 0;let a = warning[message.guild.id].warns
+a.forEach(w=>{
+if(w.user !== user.id) return
+delete w.user;delete w.reason;delete w.id;delete w.by;delete w.time;
+C++;
+})
+if(C === 0) return message.channel.send(`**:mag: I can't find the warning that you're looking for.**`)
+return message.channel.send('**✅ '+C+' warnings has been removed.**');
+};
+if(args == 'all') {
+let c = 0;let W = warning[message.guild.id].warns; 
+W.forEach(w =>{if(w.id !== undefined) c++;})
+warning[message.guild.id] = {warns:[]};
+fs.writeFile("./warning.json", JSON.stringify(warning),(err)=>{if(err)console.error(err)})
+fs.writeFile("./warning.json", JSON.stringify(warning),(err)=>{if(err)console.error(err)})
+return message.channel.send('**✅ '+c+' warnings has been removed.**')
+}if(isNaN(args)) return message.channel.send(
+    `**:rolling_eyes: Please specify warning number or user mention or (all) to delete all warnings.**`);
+let W = warning[message.guild.id].warns;
+let find = false;
+W.forEach(w =>{
+if(w.id == args) {
+delete w.user;delete w.reason;delete w.id;delete w.by;delete w.time;
+find = true;return message.channel.send('**✅ 1 warnings has been removed.**')
+}});if(find == false) return message.channel.send(`**:mag: I can't find the warning that you're looking for.**`)}});
+
+
+
+
+
 client.on("message", async message => {
 
 	if (message.author.bot) return
