7ab09e4c5830b763e52e4216a408c72f27f9e353
Glitch (db-tr) <none>
2020-03-02 13:44:01 +0000

    📹💐 Checkpoint ./index.js:9017739/45 ./html/botekle.ejs:3162708/11

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ client.profil = profil
 client.ayarlar = {
   "prefix": "!", //prefix
   "oauthSecret": "fNfWmTwu6bVfD7dHTgEYec1G5EDrhJ1d", //bot secreti
-	"callbackURL": "https://snowbotlist.tk/callback", //benim sitenin urlsini kendin ile değiş "/callback" kalacak!
+	"callbackURL": "https://snowbotslist.me/callback", //benim sitenin urlsini kendin ile değiş "/callback" kalacak!
 	"kayıt": "683457272071979237", //onaylandı, reddedildi, başvuru yapıldı falan kayıtların gideceği kanalın ID'ini yazacaksın
   "renk": "#7289DA" //embedların rengini burdan alıo can sıkıntısdna yapılmış bişe falan fln
 };
